27e7aa75036049618dea952c9540ba2af43f9457
pi <nhuby@pi.com>
2018-03-19 21:07:00 +0100

    Ajout query pour IPCKEY

--- IpckeyRestModule.rb
+++ IpckeyRestModule.rb
@@ -5,9 +5,18 @@ include WEBrick
 
 class IpckeyServlet < HTTPServlet::AbstractServlet
 def do_GET(req,resp)
-        data = File.read('data/ipckeyTuxedo1.json')
-        resp['Content-Type'] = 'application/json'
-        resp.body = data
-        raise HTTPStatus::OK
+        if req.query['tuxedo']
+            tuxedo = req.query['tuxedo'].upcase
+            filename = "data/ipckey"+tuxedo+".json"
+            if File.file?(filename)        
+                data = File.read(filename)
+                resp['Content-Type'] = 'application/json'
+                resp.body = data
+                raise HTTPStatus::OK
+            else
+                resp.body = "#{filename} Not found"
+                raise HTTPStatus::NotFound
+            end
+        end
     end
 end
--- RefreshRestModule.rb
+++ RefreshRestModule.rb
@@ -4,14 +4,23 @@ require 'json'
 include WEBrick
 
 class RefreshServlet < HTTPServlet::AbstractServlet
+   @@time_ended = true
+	
    def do_GET(req,resp)
-	resp.body = getTime()
-        raise HTTPStatus::OK
+	i = 1
+	while @@time_ended 
+ 	   resp.body = "Loading ... #{i}" 
+	   #getTime()
+	   sleep(1)
+	   i=i+1
+           raise HTTPStatus::OK
+	end
    end
 
     def getTime()
       time = Time.new
       sleep(5)
+      @@time_ended = false
       return time.to_s
     end 
 end
